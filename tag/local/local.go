c2dc37f08e278d26b3087c1e522b0a0d09b6e1d6
Ivan Ilves <ivan.ilves@gmail.com>
2017-08-20 15:20:35 +0200

    Detect Docker API version

--- tag/local/local.go
+++ tag/local/local.go
@@ -1,15 +1,59 @@
 package local
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
 	"strings"
+	"time"
 
 	// This "Moby" thing does not work for me...
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 
+	"github.com/tv42/httpunix"
 	"golang.org/x/net/context"
 )
+
+const dockerSocket = "/var/run/docker.sock"
+
+type apiVersionResponse struct {
+	ApiVersion string `json:"ApiVersion"`
+}
+
+func getApiTransport() *httpunix.Transport {
+	t := &httpunix.Transport{
+		DialTimeout:           200 * time.Millisecond,
+		RequestTimeout:        2 * time.Second,
+		ResponseHeaderTimeout: 2 * time.Second,
+	}
+	t.RegisterLocation("docker", dockerSocket)
+
+	return t
+}
+
+func parseApiVersionJSON(data io.ReadCloser) (string, error) {
+	v := apiVersionResponse{}
+
+	err := json.NewDecoder(data).Decode(&v)
+	if err != nil {
+		return "", err
+	}
+
+	return v.ApiVersion, nil
+}
+
+func detectApiVersion() (string, error) {
+	hc := http.Client{Transport: getApiTransport()}
+
+	resp, err := hc.Get("http+unix://docker/version")
+	if err != nil {
+		return "", err
+	}
+
+	return parseApiVersionJSON(resp.Body)
+}
 
 func newImageListOptions(repo string) (types.ImageListOptions, error) {
 	repoFilter := "reference=" + repo
@@ -49,7 +93,13 @@ func extractTagNames(repoTags []string, repo string) []string {
 
 func FetchTags(repo string) (map[string]string, error) {
 	ctx := context.Background()
-	cli, err := client.NewEnvClient()
+
+	apiVersion, err := detectApiVersion()
+	if err != nil {
+		return nil, err
+	}
+
+	cli, err := client.NewClient("unix://"+dockerSocket, apiVersion, nil, nil)
 	if err != nil {
 		return nil, err
 	}
