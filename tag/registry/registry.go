9c78025b106f6eb872c510ba26643b89a53faa72
Ivan Ilves <ivan.ilves@schibsted.com>
2017-09-06 15:12:48 +0200

    Make linting great again

--- tag/registry/registry.go
+++ tag/registry/registry.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 )
 
+// TraceRequests defines if we should print out HTTP request URLs and response headers/bodies
 var TraceRequests = false
 
 func getAuthorizationType(authorization string) string {
