71acf534efabbdb0c807ce4468bb548df68e3baf
Ivan Ilves <ivan.ilves@schibsted.com>
2017-09-23 10:48:02 +0200

    Filter images by tag with regular expressions :tada:

--- main_test.go
+++ main_test.go
@@ -69,7 +69,97 @@ func TestGetAuthorization(t *testing.T) {
 	}
 }
 
+func TestTrimFilter(t *testing.T) {
+	flag.Parse()
+	if *runIntegrationTests {
+		t.SkipNow()
+	}
+
+	expected := []struct {
+		repoWithFilter string
+		repo           string
+		filter         string
+		iserr          bool
+	}{
+		{"nginx", "nginx", ".*", false},
+		{"registry.hipster.io/hype/sdn", "registry.hipster.io/hype/sdn", ".*", false},
+		{"mesosphere/mesos~/^1\\.[0-9]+\\.[0-9]+$/", "mesosphere/mesos", "^1\\.[0-9]+\\.[0-9]+$", false},
+		{"registry.hipster.io/hype/drone~/v[0-9]+$/", "registry.hipster.io/hype/drone", "v[0-9]+$", false},
+		{"bogohost:5000/hype/drone~/v[0-9]+$/", "bogohost:5000/hype/drone", "v[0-9]+$", false},
+		{"registry.clown.bad/cache/merd~x[0-9]", "", "", true},
+		{"cabron/~plla~x~", "", "", true},
+	}
+
+	for _, e := range expected {
+		repo, filter, err := trimFilter(e.repoWithFilter)
+
+		if repo != e.repo {
+			t.Fatalf(
+				"Unexpected repository name '%s' trimmed from '%s' (expected: '%s')",
+				repo,
+				e.repoWithFilter,
+				e.repo,
+			)
+		}
+
+		if filter != e.filter {
+			t.Fatalf(
+				"Unexpected repository filter '%s' trimmed from '%s' (expected: '%s')",
+				filter,
+				e.repoWithFilter,
+				e.filter,
+			)
+		}
+
+		iserr := err != nil
+		if iserr != e.iserr {
+			t.Fatalf("Passing badly formatted repository '%s' should trigger an error", e.repoWithFilter)
+		}
+	}
+}
+
+func TestMatchesFilter(t *testing.T) {
+	flag.Parse()
+	if *runIntegrationTests {
+		t.SkipNow()
+	}
+
+	expected := []struct {
+		s       string
+		pattern string
+		matched bool
+	}{
+		{"latest", "^latest$", true},
+		{"v1.0.1", "^v1\\.0\\.1$", true},
+		{"barbos", ".*", true},
+		{"3.4", "*", false},
+	}
+
+	for _, e := range expected {
+		matched := matchesFilter(e.s, e.pattern)
+
+		action := "should"
+		if !e.matched {
+			action = "should not"
+		}
+
+		if matched != e.matched {
+			t.Fatalf(
+				"String '%s' %s match pattern '%s'",
+				e.s,
+				action,
+				e.pattern,
+			)
+		}
+	}
+}
+
 func TestGetRegistryName(t *testing.T) {
+	flag.Parse()
+	if *runIntegrationTests {
+		t.SkipNow()
+	}
+
 	expected := map[string]string{
 		"mesosphere/marathon":             dockerHub,
 		"bogohost/my/inner/troll":         dockerHub,
