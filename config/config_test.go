c4aea404d3085b1bb9de46f4068ce090cdbf17e2
Ivan Ilves <ivan.ilves@schibsted.com>
2018-04-08 07:16:45 +0200

    NORELEASE: Rename `c` => `yc` in `config_test.go`

--- config/config_test.go
+++ config/config_test.go
@@ -17,37 +17,37 @@ var expectedRepositories = []string{
 func TestLoadYAMLFile(t *testing.T) {
 	assert := assert.New(t)
 
-	c, err := LoadYAMLFile("../fixtures/config/config.yaml")
+	yc, err := LoadYAMLFile("../fixtures/config/config.yaml")
 
-	assert.NotNil(c, "should load config from valid config file")
+	assert.NotNil(yc, "should load config from valid config file")
 
 	assert.Nil(err, "should NOT give an error while loading valid config file")
 
-	if c != nil {
-		assert.Equal(expectedRepositories, c.Repositories)
+	if yc != nil {
+		assert.Equal(expectedRepositories, yc.Repositories)
 	}
 }
 
 func TestLoadYAMLFile_Shared(t *testing.T) {
 	assert := assert.New(t)
 
-	c, err := LoadYAMLFile("../fixtures/config/config.yaml.shared")
+	yc, err := LoadYAMLFile("../fixtures/config/config.yaml.shared")
 
-	assert.NotNil(c, "should load config from valid config file shared with others")
+	assert.NotNil(yc, "should load config from valid config file shared with others")
 
 	assert.Nil(err, "should NOT give an error while loading valid shared config file")
 
-	if c != nil {
-		assert.Equal(expectedRepositories, c.Repositories)
+	if yc != nil {
+		assert.Equal(expectedRepositories, yc.Repositories)
 	}
 }
 
 func TestLoadYAMLFile_Invalid(t *testing.T) {
 	assert := assert.New(t)
 
-	c, err := LoadYAMLFile("../fixtures/config/config.yaml.invalid")
+	yc, err := LoadYAMLFile("../fixtures/config/config.yaml.invalid")
 
-	assert.Nil(c, "should NOT load config from invalid config file")
+	assert.Nil(yc, "should NOT load config from invalid config file")
 
 	assert.NotNil(err, "should give an error while trying to load invalid config file")
 }
@@ -55,9 +55,9 @@ func TestLoadYAMLFile_Invalid(t *testing.T) {
 func TestLoadYAMLFile_Irrelevant(t *testing.T) {
 	assert := assert.New(t)
 
-	c, err := LoadYAMLFile("../fixtures/config/config.yaml.irrelevant")
+	yc, err := LoadYAMLFile("../fixtures/config/config.yaml.irrelevant")
 
-	assert.Nil(c, "should NOT load config from irrelevant config file")
+	assert.Nil(yc, "should NOT load config from irrelevant config file")
 
 	assert.NotNil(err, "should give an error while trying to load irrelevant config file")
 }
@@ -65,9 +65,9 @@ func TestLoadYAMLFile_Irrelevant(t *testing.T) {
 func TestLoadYAMLFile_NonExisting(t *testing.T) {
 	assert := assert.New(t)
 
-	c, err := LoadYAMLFile("/i/do/not/exist/sorry")
+	yc, err := LoadYAMLFile("/i/do/not/exist/sorry")
 
-	assert.Nil(c, "should NOT load config from non-existing config file")
+	assert.Nil(yc, "should NOT load config from non-existing config file")
 
 	assert.NotNil(err, "should give an error while trying to load non-existing config file")
 }
