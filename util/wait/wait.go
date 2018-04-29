51dce409149f7137ba20113eaee732324c81dd17
Ivan Ilves <ivan.ilves@schibsted.com>
2018-04-29 10:47:08 +0200

    IMPROVE: Tolerate errors on `Pull` and `Push` actions
    As stated in https://github.com/ivanilves/lstags/issues/146
    we need to try harder while pulling or pushing Docker images.

--- util/wait/wait.go
+++ util/wait/wait.go
@@ -1,5 +1,7 @@
 package wait
 
+import "fmt"
+
 // Until iterates over buffered error channel and:
 // * upon receiving non-nil value from the channel, makes an early return with this value
 // * if no non-nil values were received from iteration over the channel, it just returns nil
@@ -20,4 +22,30 @@ func Until(done chan error) error {
 	}
 
 	return nil
+}
+
+// WithTolerance is the same as Until, but it does not return immediately on errors
+// rather loops through all channel capacity returning "composite" error in the end.
+func WithTolerance(done chan error) error {
+	var errMessage string
+
+	i := 0
+
+	for err := range done {
+		if err != nil {
+			errMessage = errMessage + err.Error() + "\n"
+		}
+
+		i++
+
+		if i >= cap(done) {
+			close(done)
+		}
+	}
+
+	if len(errMessage) == 0 {
+		return nil
+	}
+
+	return fmt.Errorf(errMessage)
 }
