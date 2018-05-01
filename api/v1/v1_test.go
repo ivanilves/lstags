4567a724584ff46f6ff504d3e9dd2a34ec0e6bd5
Ivan Ilves <ivan.ilves@schibsted.com>
2018-05-01 20:14:09 +0200

    chore(registry): move to `api/internal` namespace

--- api/v1/v1_test.go
+++ api/v1/v1_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/ivanilves/lstags/api/registry"
+	"github.com/ivanilves/lstags/api/internal/registry"
 	"github.com/ivanilves/lstags/repository"
 )
 
